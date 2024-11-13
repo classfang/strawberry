c356f635ae3dfb7199c38ca40d52f1c9c75202cc
Junki <fangjunjievip@hotmail.com>
2024-11-13 14:45:35 +0800

    fix: MacOS新增状态标题

--- src/renderer/src/components/title/TitleBar.vue
+++ src/renderer/src/components/title/TitleBar.vue
@@ -3,13 +3,23 @@ import { AppConfig } from '@renderer/config/AppConfig'
 import { useStore } from '@renderer/store/store'
 
 // 仓库
-const { chatSessionStore } = useStore()
+const { chatSessionStore, appStateStore } = useStore()
 </script>
 
 <template>
   <div class="title-bar drag-area">
     <div class="title single-line-ellipsis">
-      {{ chatSessionStore.getActiveSession?.name || AppConfig.name }}
+      <template v-if="appStateStore.chatLoadingFlag">
+        <span v-if="appStateStore.chatAnswerFlag" class="title-state-answering">
+          {{ $t('app.state.answering') }}
+        </span>
+        <span v-else class="title-state-wait">
+          {{ $t('app.state.waitAnswer') }}
+        </span>
+      </template>
+      <span v-else>
+        {{ chatSessionStore.getActiveSession?.name || AppConfig.name }}
+      </span>
     </div>
   </div>
 </template>
@@ -30,6 +40,24 @@ const { chatSessionStore } = useStore()
     font-size: var(--el-font-size-small);
     text-align: center;
     font-weight: var(--el-font-weight-primary);
+
+    @keyframes alternate-breathing {
+      0%,
+      100% {
+        opacity: 1;
+      }
+      50% {
+        opacity: 0.5;
+      }
+    }
+
+    .title-state-wait {
+      animation: alternate-breathing 1200ms ease-in-out infinite;
+    }
+
+    .title-state-answering {
+      animation: alternate-breathing 800ms ease-in-out infinite;
+    }
   }
 }
 </style>
